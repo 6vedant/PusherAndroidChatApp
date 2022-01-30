d066ccff697f3c7ea59706b53ce4bb97a8c6f10b
vj <vedantj@nova.tech>
2022-01-30 16:04:05 +0530

    initial commit

---
+++ app/src/main/java/com/kehinde/pusher_chat_test/GlobalApplication.kt
@@ -0,0 +1,17 @@
+package com.kehinde.pusher_chat_test
+
+import android.app.Application
+import android.content.Context
+import io.stipop.Stipop
+class GlobalApplication : Application() {
+    companion object {
+        lateinit var instance: GlobalApplication
+            private set
+    }
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        Stipop.configure(this)
+    }
+    fun context(): Context = applicationContext
+}
